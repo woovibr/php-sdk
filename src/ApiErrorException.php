438b1106b7d51c781d1f94217b1e6d6fa6d70cb2
Cristiano <cris.moraes.ds@gmail.com>
2023-06-07 19:41:51 -0300

    fix project namespace

--- src/ApiErrorException.php
+++ src/ApiErrorException.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Openpix\PhpSdk;
+namespace OpenPix\PhpSdk;
 
 use Exception;
 
--- src/Client.php
+++ src/Client.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Openpix\PhpSdk;
+namespace OpenPix\PhpSdk;
 
 use OpenPix\PhpSdk\RequestTransport;
 use OpenPix\PhpSdk\Resources\Customers;
