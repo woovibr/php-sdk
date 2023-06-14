7d2e36e7596ddc384afcd5006c34a8f6cc48d1de
Cristiano <cris.moraes.ds@gmail.com>
2023-06-14 15:33:05 -0300

    operations on refunds

--- src/Resources/Refunds.php
+++ src/Resources/Refunds.php
@@ -6,30 +6,90 @@ use OpenPix\PhpSdk\Paginator;
 use OpenPix\PhpSdk\Request;
 use OpenPix\PhpSdk\RequestTransport;
 
+/**
+ * Operations on refunds.
+ */
 class Refunds
 {
+    /**
+     * Transport used to send HTTP requests.
+     */
     private RequestTransport $requestTransport;
 
+    /**
+     * Create a new Refunds instance.
+     *
+     * @param RequestTransport Used to send HTTP requests for Refunds API.
+     */
     public function __construct(RequestTransport $requestTransport)
     {
         $this->requestTransport = $requestTransport;
     }
 
     /**
-     * @param array<mixed> $params
+     * Return an {@see Paginator} with API refund list results wrapped.
+     *
+     * ```php
+     * $paginator = $client->refunds()->list();
+     *
+     * foreach ($paginator as $result) {
+     *      foreach ($result["refunds"] as $refund) {
+     *          $refund; // array
+     *          $refund["value"]; // int
+     *          $refund["correlationID"]; // string
+     *
+     *          // Enum: "IN_PROCESSING", "REFUNDED" or "NOT_ACCOMPLISHED"
+     *          $refund["status"];
+     *
+     *          // Unique refund ID for this pix refund
+     *          $refund["refundId"]; // string
+     *
+     *          // and more...
+     *      }
+     * }
+     * ```
+     *
+     * @link https://developers.openpix.com.br/api#tag/refund/paths/~1api~1v1~1refund/get
+     *
+     * @param array<string, mixed> $queryParams
+     *
+     * @return Paginator
      */
     public function list(array $params = []): Paginator
     {
         $request = (new Request())
             ->method("GET")
             ->path("/refund")
-            ->queryParams($params);
+            ->queryParams($queryParams);
 
         return new Paginator($this->requestTransport, $request);
     }
 
     /**
-     * @return array<mixed>
+     * Get one refund by ID.
+     *
+     * ## Usage
+     * ```php
+     * $result = $client->refunds()->getOne("refundID");
+     *
+     * $result["refund"]; // array
+     * $result["refund"]["value"]; // int
+     * $result["refund"]["correlationID"]; // string
+     *
+     * // Enum: "IN_PROCESSING", "REFUNDED" or "NOT_ACCOMPLISHED"
+     * $result["refund"]["status"];
+     *
+     * // Unique refund ID for this pix refund
+     * $result["refund"]["refundId"]; // string
+     *
+     * // and more...
+     * ```
+     *
+     * @link https://developers.openpix.com.br/api#tag/refund/paths/~1api~1v1~1refund~1%7Bid%7D/get
+     *
+     * @param string $refundID Refund ID or correlation ID.
+     *
+     * @return array<string, mixed> Result from API.
      */
     public function getOne(string $refundID): array
     {
@@ -41,8 +101,38 @@ class Refunds
     }
 
     /**
-     * @param array<mixed> $refund
-     * @return array<mixed>
+     * Create a new refund.
+     *
+     * ## Usage
+     * ```php
+     * $result = $client->refunds()->create([
+     *      "value" => 0,
+     *      // Your transaction ID, or endToEnd ID, to keep track of this refund.
+     *      "transactionEndToEndId" => "",
+     *      // Your correlation ID, unique identifier refund
+     *      "correlationID" => "",
+     *      // Comment of this refund. Maximum length of 140 characters.
+     *      "comment" => "",
+     * ]);
+     *
+     * $result["refund"]; // array
+     * $result["refund"]["value"]; // int
+     * $result["refund"]["correlationID"]; // string
+     *
+     * // Enum: "IN_PROCESSING", "REFUNDED" or "NOT_ACCOMPLISHED"
+     * $result["refund"]["status"];
+     *
+     * // Unique refund ID for this pix refund
+     * $result["refund"]["refundId"]; // string
+     *
+     * // and more...
+     * ```
+     *
+     * @link https://developers.openpix.com.br/api#tag/refund/paths/~1api~1v1~1refund/post
+     *
+     * @param array<string, mixed> $refund Refund data.
+     *
+     * @return array<string, mixed> Result from API.
      */
     public function create(array $refund): array
     {
